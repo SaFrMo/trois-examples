3b0255adfdd31b07489bd418333e5a59c76f4312
Sander Moolin <sandermoolin@gmail.com>
2021-03-19 15:32:31 -0400

    trois-examples

--- src/components/Lightboxes/Lightboxes.vue
+++ src/components/Lightboxes/Lightboxes.vue
@@ -1,47 +1,80 @@
 <template>
     <Renderer ref="renderer" resize antialias orbit-ctrl>
-        <Camera :position="{ y: height * -0.5, z: 10 }" ref="camera" />
+        <Camera :position="{ x: 15, y: 20, z: 30 }" />
         <Scene>
+            <!-- lights -->
             <RectAreaLight
-                v-for="i in rows * columns"
+                helper
+                :rotation="{ y: Math.PI }"
+                :width="4"
+                :height="height"
+                v-for="(item, i) in grid.x * grid.y"
                 :key="i"
                 :position="{
                     x:
-                        ((i % rows) - this.rows * 0.5) * xSpacer +
-                        (Math.floor(-i / columns) % 2 ? xSpacer * 0.55 : 0),
-                    z: Math.floor(-i / columns) * zSpacer + 40,
+                        (i % grid.x) * spacer.x +
+                        (Math.floor(i / grid.y) % 2 ? spacer.x * 0.5 : 0) -
+                        spacer.x * grid.x * 0.5,
+                    z: Math.floor(i / grid.y) * -spacer.y,
                 }"
-                helper
-                :width="7"
-                :height="height"
-                :rotation="{ y: Math.PI }"
-                :intensity="0.3"
+                :ref="setItemRef"
             />
+
+            <!-- ground -->
+            <Plane
+                :position="{ y: -height * 0.5 }"
+                :rotation="{ x: -Math.PI * 0.5 }"
+                :scale="{ x: 300, y: 300 }"
+            >
+                <StandardMaterial color="#ff0000" :roughness="0.2" />
+            </Plane>
         </Scene>
+
         <EffectComposer>
             <RenderPass />
-            <UnrealBloomPass :strength="1" />
-            <!-- <HalftonePass :radius="1" :scatter="0" /> -->
+            <UnrealBloomPass :strength="0.4" />
             <FXAAPass />
         </EffectComposer>
     </Renderer>
 </template>
 
 <script>
-import { Vector3 } from 'three'
+import { Vector2 } from 'three'
+import { tween, chain, delay } from 'popmotion'
 
 export default {
     data() {
         return {
-            rows: 10,
-            columns: 10,
-            xSpacer: 20,
-            zSpacer: 40,
-            height: 20,
+            height: 8,
+            grid: new Vector2(4, 4),
+            spacer: new Vector2(10, 10),
+            lights: [],
         }
     },
     mounted() {
-        this.$refs.camera.camera.lookAt(new Vector3(0, 20, -200))
+        this.lights.forEach((l) => (l.intensity = 0))
+        for (let i = 0; i < this.grid.x * this.grid.y; i++) {
+            chain(
+                delay((i + 1) * 300),
+                tween({
+                    from: 0.1,
+                    to: 1,
+                    duration: 1000,
+                    flip: Infinity,
+                })
+            ).start((v) => {
+                if (this.lights[i]) {
+                    this.lights[i].intensity = v || 0
+                }
+            })
+        }
+    },
+    methods: {
+        setItemRef(el) {
+            if (el) {
+                this.lights.push(el.light)
+            }
+        },
     },
 }
 </script>
