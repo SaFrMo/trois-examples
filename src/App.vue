1d6b8e00e605642c188a1dee6ffbfd623d32e6f7
Sander Moolin <sandermoolin@gmail.com>
2021-03-24 17:57:57 -0400

    mars updates

--- src/App.vue
+++ src/App.vue
@@ -32,7 +32,7 @@ export default {
     },
     data() {
         return {
-            selected: 'MarsDemo',
+            selected: '',
         }
     },
     mounted() {
--- src/components/MarsDemo.vue
+++ src/components/MarsDemo.vue
@@ -1,14 +1,36 @@
 <template>
-    <Renderer ref="renderer" resize>
-        <Camera ref="camera" :fov="11" :position="{ x: 1, y: 0.45, z: 5 }" />
+    <Renderer ref="renderer" resize shadow>
+        <Camera ref="camera" :fov="11" :position="cameraPos" />
         <Scene>
-            <PointLight color="goldenrod" :position="{ y: 1, x: 5, z: 2 }" />
+            <!-- lights -->
+            <PointLight
+                cast-shadow
+                color="goldenrod"
+                :position="{ x: 4, y: 2, z: 2 }"
+                ref="sun"
+            />
             <AmbientLight :intensity="0.1" />
-            <Icosahedron :detail="15" ref="planet">
+
+            <!-- planet -->
+            <Icosahedron :detail="15" ref="planet" cast-shadow receive-shadow>
                 <StandardMaterial>
                     <Texture src="/images/mars.jpg" />
                 </StandardMaterial>
             </Icosahedron>
+
+            <!-- square -->
+            <!-- revolves around planet -->
+            <Group ref="container">
+                <Box
+                    ref="box"
+                    :position="{ x: 1.2 }"
+                    :size="0.05"
+                    cast-shadow
+                    receive-shadow
+                >
+                    <StandardMaterial />
+                </Box>
+            </Group>
         </Scene>
         <EffectComposer>
             <RenderPass />
@@ -19,26 +41,41 @@
 </template>
 
 <script>
-import { tween } from 'popmotion'
-import { Vector3 } from 'three'
+import { Vector2, Vector3, Object3D } from 'three'
+const body = new Object3D()
+const up = new Vector3(0, 1, 0)
 
 export default {
+    data() {
+        return {
+            cameraPos: new Vector3(1, 0.45, 5), //0, 0.45, 15),
+        }
+    },
     mounted() {
+        this.$refs.container.o3d.rotation.z = 0.4
+        this.$refs.container.o3d.rotation.y = -0.5
+        this.$refs.container.o3d.rotation.order = 'ZYX'
+
+        // adjust shadow
+        const sun = this.$refs.sun.light
+        sun.shadow.camera.near = 1
+        sun.shadow.camera.far = 5
+        sun.shadow.map.dispose()
+        sun.shadow.map = null
+        sun.shadow.mapSize = new Vector2(2048, 2048)
+
+        // update
         this.$refs.renderer.three.onBeforeRender(this.update)
-        tween({
-            from: new Vector3(1, 0.45, 5),
-            to: new Vector3(0.8, -0.1, 5),
-            duration: 14000,
-            flip: Infinity,
-        }).start((v) => {
-            if (this && this.$refs && this.$refs.camera) {
-                this.$refs.camera.camera.position.set(v.x, v.y, v.z)
-            }
-        })
     },
     methods: {
         update() {
-            this.$refs.planet.mesh.rotation.set(0, Date.now() * 0.00001, 0)
+            const now = Date.now()
+
+            // rotate planet
+            this.$refs.planet.mesh.rotation.set(0, now * 0.00001, 0)
+
+            // revolve box
+            this.$refs.container.o3d.rotation.y = now * 0.001
         },
     },
 }
